540e838bf21106a8fe9524b2a9b5d54c4cb904e9
anousheh21 <anousheh1@gmail.com>
2024-02-24 13:13:00 +0000

    first commit

---
+++ PhotoNotes/PhotoNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoNotesApp.swift
+//  PhotoNotes
+//
+//  Created by Anousheh Hasan on 24/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
