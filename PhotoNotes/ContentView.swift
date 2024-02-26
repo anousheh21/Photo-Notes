688ee3ea96cda39a9312e4170be7959df8e35cca
anousheh21 <anousheh1@gmail.com>
2024-02-26 07:59:15 +0000

    Added functionality to add photos to a list

--- PhotoNotes/ContentView.swift
+++ PhotoNotes/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var pickerItem: PhotosPickerItem?
-    @State private var selectedImage: Image?
-    @State private var images: [Image] = []
+    @State private var selectedImage: UIImage?
+    @State  private var images: [UIImage] = []
     
     var body: some View {
         NavigationStack {
@@ -20,9 +20,14 @@ struct ContentView: View {
                     if let selectedImage {
                         // Get rid of the code inside this if block and instead place a function, that will also be run when the user taps the plus button in the toolbar
                         // PUT LIST HERE???
-                        selectedImage
-                            .resizable()
-                            .scaledToFit()
+                        List {
+                            ForEach(images.indices, id: \.self) { index in
+                                Image(uiImage: images[index])
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(width: 100, height: 100)
+                            }
+                        }
                     } else {
                         ContentUnavailableView("Photo Notes Library Empty", systemImage: "photo.badge.plus", description: Text("Tap to import photo"))
                     }
@@ -33,7 +38,7 @@ struct ContentView: View {
             .toolbar {
                 ToolbarItem {
                     Button {
-                        // add image function goes here?
+                        loadImage()
                     } label: {
                         Image(systemName: "plus")
                     }
@@ -45,9 +50,23 @@ struct ContentView: View {
     // Function to load image into app
     func loadImage() {
         Task {
-            selectedImage = try await pickerItem?.loadTransferable(type: Image.self)
+            if let pickerItem = pickerItem {
+                do {
+                    // Attempt to load the selected item as Data
+                    let data = try await pickerItem.loadTransferable(type: Data.self)
+                    // Convert the loaded Data into a UIImage
+                    if let uiImage = UIImage(data: data!) {
+                        selectedImage = uiImage
+                        images.append(uiImage)
+                    }
+                } catch {
+                    // Handle any errors that occur during loading
+                    print("Failed to load image: \(error)")
+                }
+            }
         }
     }
+
 }
 
 #Preview {
